78faabc740066784d608dbeb99e513059aaac582
lgomezr16 <lorenzo_001@outlook.com>
2023-04-10 18:44:05 +0200

    Creacion de repositorio con codigo de Programamcion

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+
+fun main(args: Array<String>) {
+    var cad : String = ""
+    var letra : Char = ' '
+
+    println("Dime la cadena")
+    cad = readln().trim().uppercase()
+    println("Dime la letra que quieres buscar")
+    letra = readln().get(0)
+
+    println("La letra ${letra} aparece ${buscarLetra(cad, letra)} veces")
+
+}
+fun buscarLetra(cad : String, letra : Char) : Int{
+    var cont : Int = 0
+    var posicion : Int = 0
+    var buscador : Int = 0
+
+    do {
+        buscador = cad.indexOf(letra, posicion, true)
+        if (buscador != -1){
+            posicion = buscador + 1
+            cont ++
+        }
+    }while (buscador != -1)
+    return cont
+}
